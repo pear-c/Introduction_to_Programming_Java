d049493616758247e637558a4f9e24dfc2e125b6
pear_c <zxcv9676@naver.com>
2025-02-25 13:32:26 +0900

    [Introduction To Programming Using Java] Chapter.02-1 - The Basic Java Application

---
+++ src/main/java/Chap_02/Quiz_2_1.java
@@ -0,0 +1,32 @@
+package Chap_02;
+
+public class Quiz_2_1 {
+    static void Problem01() {
+        System.out.println("Hello World!");
+    }
+
+    static void Problem02() {
+        System.out.println("배성환");
+    }
+
+    // " 누락
+    static void Problem03() {
+        System.out.println("This program has an answer");
+    }
+
+    static void Problem04() {
+        System.out.println("Hello, Java!");
+        System.out.println("Let's start coding.");
+    }
+
+    // 프로그램의 이름: HelloWorld
+    // 프로그램의 시작점
+    // "Hello, World!" 메시지를 출력
+    static void Problem05() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
