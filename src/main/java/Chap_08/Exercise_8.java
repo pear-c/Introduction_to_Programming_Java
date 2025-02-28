d2e6450c04d50f7aafe9114242ff94e8caed873a
pear_c <zxcv9676@naver.com>
2025-02-28 17:51:00 +0900

    Chapter.08) Section 6 - Programming Exercises

--- src/main/java/Chap_08/Exercise_8.java
+++ src/main/java/Chap_08/Exercise_8.java
@@ -1,6 +1,11 @@
 package Chap_08;
 
+import java.util.Scanner;
+
 public class Exercise_8 {
+    static Scanner sc = new Scanner(System.in);
+
+    // 연습문제 8.1 : 아래 서브루틴을 사용하여 방정식 출력하는 프로그램
     static public double root (double A, double B, double C) throws IllegalArgumentException {
         if(A == 0) {
             throw new IllegalArgumentException("A can't be zero");
@@ -12,10 +17,26 @@ public class Exercise_8 {
             }
             return (-B + Math.sqrt(disc)) / (2*A);
         }
-
     }
+    static void Problem01() {
+        boolean again = true;
+        while(again) {
+            System.out.print("정수 3개 입력: ");
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int C = sc.nextInt();
+            try {
+                System.out.println("근: " + root (A, B, C));
+            } catch(IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.print("계속 할까요? (true/false): ");
+            again = sc.nextBoolean();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(root(3, 5, 1));
+        Problem01();
 
     }
 }
